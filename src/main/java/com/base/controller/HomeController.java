8890bfa52e82236368ea5a40e18ca1e12e504eff
fms <liuli_erp@163.com>
2016-08-22 15:56:31 +0800

    Update HomeController.java

--- src/main/java/com/base/controller/HomeController.java
+++ src/main/java/com/base/controller/HomeController.java
@@ -1,5 +1,23 @@
 package com.base.controller;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tr.ihome.service.HomeService;
+
+@Controller
+@RequestMapping(value="/home")
 public class HomeController {
-
+	 private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
+	@Autowired
+	private HomeService homeService;
+    @RequestMapping(value="index")
+	public String index(){
+      System.out.println(">>>>>>show");
+      LOG.info("in to log{},log info:{}","YES","showindex");
+      homeService.showindexs();
+       return "ok";	
+    }
 }
