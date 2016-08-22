394c274fbaf4a8fbe7593cf6ee10d7781b9e3479
fms <liuli_erp@163.com>
2016-08-22 15:51:46 +0800

    Update TestMapper.java

--- src/main/java/com/base/service/repository/TestMapper.java
+++ src/main/java/com/base/service/repository/TestMapper.java
@@ -1,5 +1,17 @@
-package com.base.service.repository;
+package com.tr.ihome.service.repository;
 
-public class TestMapper {
+import java.util.List;
 
+import com.tr.ihome.persistence.Test;
+
+/**
+ * 
+ * TestMapper数据库操作接口类
+ * 
+ **/
+
+public interface TestMapper{
+    public List<Test> showindex();
+    public Integer updateshow();
+    public Integer updateindex();
 }
